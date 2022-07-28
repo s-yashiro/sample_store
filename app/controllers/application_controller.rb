267cb3428ee1d0bcb207238e69e46d2e931ad4dc
s-yashiro <satoshi.yashiro@gmail.com>
2022-07-28 20:26:34 +0900

    Add some work arounds for device on Rails 7..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseHackFakeSession
+
+  before_action do
+    I18n.locale = :ja
+  end
 end
